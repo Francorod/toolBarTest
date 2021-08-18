8e3377c101b85d64bd6dd933fe4a2770ae03332d
Franco Rod <81878824+Francorod@users.noreply.github.com>
2021-08-18 08:29:54 -0700

    Initial Commit

---
+++ Shared/toolBarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  toolBarTestApp.swift
+//  Shared
+//
+//  Created by Franco Rodrigues on 8/18/21.
+//
+
+import SwiftUI
+
+@main
+struct toolBarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
