e06c8bbf45f8fd58118dbbea1c624c5637834467
Franco Rod <81878824+Francorod@users.noreply.github.com>
2022-03-14 18:52:11 -0400

    Tool bar basic

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,11 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Text("Facebook")
+                Text ("Instagram")
+                Text("TikTok")
+            }.toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Button(action: {
+                        print("Pencil button was tappped")
+                    }) {
+                        Image(systemName: "Square.and.pencil")
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Button("Close") {
+                        print("close button tapped")
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarLeading) {
+                    Button(action: {
+                        print("Message Button was tapped")
+                    }) {
+                        Image(systemName: "paperplace")
+                    }
+                }
+            }
     }
 }
-
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
